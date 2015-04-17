7047fc26988dcf18aea619318ef784224bf43e30
asholok <asholok@gmail.com>
2015-04-17 19:29:34 +0300

    add local.css

--- asholok/convertor/views.py
+++ asholok/convertor/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from models import Engine
+from my_auth.backends import PREPARED_BACKENDS
 
 def index(request):
-	return render(request, 'convertor/index.html', {'engine': Engine()})
+	return render(request, 'convertor/index.html', 
+		{'engine': Engine(), 'available_backends': PREPARED_BACKENDS}
+	)
 
 def results(request):
 	language = request.POST['language']
 	currency = request.POST['currency']
 	number = request.POST['number'].strip()
 
-	return render(request, 'convertor/index.html', {'engine': Engine(language, currency, number)})
+	return render(request, 'convertor/index.html', 
+		{'engine': Engine(language, currency, number), 'available_backends': PREPARED_BACKENDS }
+	)
 
 # Create your views here.
